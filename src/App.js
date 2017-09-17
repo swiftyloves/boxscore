5f735555b347b0babbac657fa0cfdd5483e816b4
Jie-Wei <swiftywu@yahoo.com>
2017-09-17 12:02:39 -0400

    finish game

--- src/App.js
+++ src/App.js
@@ -8,6 +8,38 @@ const Dispatcher = require('flux').Dispatcher;
 console.log(datajson)
 
 
+
+
+class Game extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount () {
+    
+  }
+
+  render() {
+    let players = [];
+    for (let i = 0; i < players.length; i++) {
+        let playerObj = this.props.data.players[i];
+        if (playerObj.player_id) {
+            // players.push(<Player player={this.props.data.players[i]}>)
+        }
+    }
+    return (
+        <div class="game">
+            <div>{this.props.data.name}</div>
+            <img src={this.props.data.home.image} alt="homeimage" />
+            <div>{this.props.data.home.name}</div>
+            <img src={this.props.data.away.image} alt="homeimage" />
+            <div>{this.props.data.away.name}</div>
+        </div>
+    );
+
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -25,6 +57,11 @@ class App extends Component {
 
   render() {
     let firstdata = this.state.data[0].name
+    let games = [];
+    for (let i=0; i < this.state.data.length; i++) {
+        games.push(<Game data={this.state.data[i]}/>);
+    }
+    console.log(games.length)
 
 
     return (
@@ -32,11 +69,12 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React 123</h2>
-          <h3>{firstdata}</h3>
+          
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div id="games">{games}</div>
       </div>
     );
   }
